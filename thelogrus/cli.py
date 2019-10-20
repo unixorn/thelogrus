5f9fa301752d3c03667d904c4ebcda259bbb9928
Joe Block <jpb@unixorn.net>
2019-10-20 10:51:46 -0600

    Update cli.find_subcommand to work in py3
    * Update to work with Python 3
    * Add exec_subcommand function
    * Eliminate pylint complaints
    * Tweak formatting in docstrings
    * rename functions per pylint

--- thelogrus/cli.py
+++ thelogrus/cli.py
@@ -20,20 +20,57 @@ Utility functions for dealing with programs.
 '''
 
 import distutils.spawn
+import os
+import sys
 
 
-def findSubCommand(args):
+def exec_subcommand():
   '''
-  Given a list ['foo','bar', 'baz'], attempts to create a command name in the
-  format 'foo-bar-baz'. If that command exists, we run it. If it doesn't, we
-  check to see if foo-bar exists, in which case we run `foo-bar baz`. We keep
-  taking chunks off the end of the command name and adding them to the argument
-  list until we find a valid command name we can run.
+  Process the command line arguments and run the appropriate subcommand.
 
-  This allows us to easily make git-style command drivers where for example we
-  have a driver script, foo, and subcommand scripts foo-bar and foo-baz, and when
-  the user types `foo bar foobar` we find the foo-bar script and run it as
-  `foo-bar foobar`
+  We want to be able to do git-style handoffs to subcommands where if we
+  do `foo blah foo bar` and the executable foo-blah-foo exists, we'll call
+  it with the argument bar.
+
+  We deliberately don't do anything with the arguments other than hand
+  them off to the subcommand. Subcommands are responsible for their
+  own argument parsing.
+
+  Use os.execvp so that subcommands inherit our stdin.
+  '''
+  try:
+    (command, args) = find_subcommand(sys.argv)
+
+    # If we can't construct a subcommand from sys.argv, it'll still be able
+    # to find this driver script, and re-running ourself isn't useful.
+    if os.path.basename(command) == sys.argv[0]:
+      print("Could not find a subcommand for %s" % ' '.join(sys.argv))
+      sys.exit(1)
+  except Exception as e:
+    print(str(e))
+    sys.exit(1)
+  args.insert(0, command)
+  sys.stdout.flush()
+  sys.stderr.flush()
+  os.execvp(command, args)
+
+
+def find_subcommand(args):
+  '''
+  Given a list ['foo','bar', 'baz'], attempts to create a command name
+  in the format 'foo-bar-baz'. If that command exists, we run it. If it
+  doesn't, we check to see if foo-bar exists, in which case we run
+  `foo-bar baz`.
+
+  We keep peeling chunks off the end of the command name and adding them
+  to the argument list until we find a valid command name we can run.
+
+  This allows us to easily make git-style command drivers where, for
+  example we have a driver script, foo, and subcommand scripts foo-bar
+  and foo-baz, and when the user types `foo bar foobar` we find the foo-bar
+  script and run it as
+
+    `foo-bar foobar`
 
   :param list|tuple args: list to try and convert to a command args pair
   :returns: command and arguments list
@@ -43,15 +80,16 @@ def findSubCommand(args):
   # If the only command we find is the first element of args, we've found the
   # driver script itself and re-executing it will cause an infinite loop, so
   # don't even look at the first element on its own.
-  for n in range(len(args) - 1):
-    command = '-'.join(args[:(len(args) - n)])
-    commandArgs = args[len(args) - n:]
-    if isProgram(command):
-      return (command, commandArgs)
-  raise StandardError("Could not find a %s subcommand executable" % command)
+  command = None
+  for i in range(len(args) - 1):
+    command = '-'.join(args[:(len(args) - i)])
+    command_arguments = args[len(args) - i:]
+    if is_program(command):
+      return (command, command_arguments)
+  raise RuntimeError("Could not find a subcommand executable for %s" % ' '.join(args))
 
 
-def isProgram(name):
+def is_program(name):
   '''
   Search for a given program in $PATH, and return True if it exists and
   is executable.
